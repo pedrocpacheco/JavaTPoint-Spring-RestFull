c9f67644c004755dafc76ea6a686f6e4ee0e04eb
pedrocpacheco <cpedropacheco@hotmail.com>
2024-03-05 15:51:41 -0300

    feat: exception for 404 error in get method

--- src/main/java/com/javatpoint/restfulwebservices/user/UserResource.java
+++ src/main/java/com/javatpoint/restfulwebservices/user/UserResource.java
@@ -25,7 +25,11 @@ public class UserResource {
 
   @GetMapping("/users/{id}")
   public User retriveUser(@PathVariable Integer id) {
-    return service.findOne(id);
+    var user = service.findOne(id);
+    if (user == null) {
+      throw new UserNotFoundException("id:" + id);
+    }
+    return user;
   }
 
   @PostMapping("/users")
