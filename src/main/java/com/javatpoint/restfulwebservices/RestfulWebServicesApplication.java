cccd3474a0aac998fc4d3dee8ceda91fa745897e
pedrocpacheco <cpedropacheco@hotmail.com>
2024-03-05 14:18:05 -0300

    config: exclude jacksopn and set debug

--- src/main/java/com/javatpoint/restfulwebservices/RestfulWebServicesApplication.java
+++ src/main/java/com/javatpoint/restfulwebservices/RestfulWebServicesApplication.java
@@ -3,7 +3,7 @@ package com.javatpoint.restfulwebservices;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication // ? - Auto Configuração
 public class RestfulWebServicesApplication {
 
 	public static void main(String[] args) {
