748032bcf1f798b3e2c6d00430042c2aefea7554
pedrocpacheco <cpedropacheco@hotmail.com>
2024-03-05 15:20:10 -0300

    feat(UserResource): add Post Method

--- src/main/java/com/javatpoint/restfulwebservices/user/UserResource.java
+++ src/main/java/com/javatpoint/restfulwebservices/user/UserResource.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @RestController
 public class UserResource {
@@ -21,6 +23,11 @@ public class UserResource {
   @GetMapping("/users/{id}")
   public User retriveUser(@PathVariable Integer id) {
     return service.findOne(id);
+  }
+
+  @PostMapping("/users")
+  public void createUser(@RequestBody User user) {
+    User savedUser = service.save(user);
   }
 
 }
